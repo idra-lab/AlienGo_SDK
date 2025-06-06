51a324405c58f33ed02cc8c0adbe490f19d0545e
JessicaVillalobos <jessica.villalobos9@gmail.com>
2025-06-06 11:16:06 +0200

    Add ROS

--- example_py/MPS_robot_sensors/simulator_interface.py
+++ example_py/MPS_robot_sensors/simulator_interface.py
@@ -1,13 +1,14 @@
 import mujoco
 from mujoco import viewer
 import numpy as np
+import rospy
 
 class MujocoSim():
     def __init__(self):
 
         self.model, self.data = self.modelData()
-        self.simulate()
         self.lim_tau
+        self.iter_ctrl
 
     def modelData(self):
         # MuJoCo robot model
@@ -20,6 +21,7 @@ class MujocoSim():
         return model_muj, data_muj
     
     def simulate(self, Kp, Kd, forward_torques):
+        j = 0
         while j < self.iter_ctrl:
             u_nominal = Kd * (- v_muj[6:]) + Kp * (qDes - q_muj[7:]) +  forward_torques
             self.data.ctrl = np.clip(u_nominal, -self.lim_tau, self.lim_tau)
@@ -27,3 +29,8 @@ class MujocoSim():
             mujoco.mj_step(self.data.model, self.data)
             q_muj = self.data.qpos.copy()
             v_muj = self.data.qvel.copy()
+
+if __name__ == '__main__':
+    mujoco_sim = MujocoSim()
+    while not rospy.is_shutdown():
+        mujoco_sim.simulate(Kp, Kd, forward_torques)
