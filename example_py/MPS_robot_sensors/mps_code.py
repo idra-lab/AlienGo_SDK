694622a5092a561b8482a9176807f9acc9476e1c
JessicaVillalobos <jessica.villalobos9@gmail.com>
2025-06-08 13:59:12 +0000

    MPS added for its use with generic_simulator_floating

--- example_py/MPS_robot_sensors/mps_code.py
+++ example_py/MPS_robot_sensors/mps_code.py
@@ -4,6 +4,10 @@ import torch
 import torch.nn as nn
 from collections import OrderedDict
 import os
+from   AlienGo_SDK.example_py.MPS_robot_sensors.utils import quat_rotate_inverse
+import matplotlib
+from matplotlib.path import Path
+from shapely.geometry import Polygon
 
 def modelData():
     # MuJoCo robot model
@@ -121,102 +125,189 @@ def orderBackup(pos):
     return pos[order_pos].detach().cpu().numpy()
 
 class MPS:
-    def __init__(self, decimation, max_pos, min_pos, torque_values, Kp_n, Kd_n, config):
+    def __init__(self, decimation, max_pos, min_pos, torque_values, Kp_n, Kd_n, scaling_qdes, default_joint_angles, scaling_factors):
         # Compute the number of MuJoCo iterations to use each network output 
-        self.iter_ctrl = decimation
+        self.iter_ctrl = 5
 
         # Parameters for the nominal policy
         self.max_pos = max_pos
         self.min_pos = min_pos
-        self.torque_values = torque_values
+        self.torque_values = 4*torque_values
         self.Kp_n = Kp_n
         self.Kd_n = Kd_n
+        self.scaling_qdes = scaling_qdes
+        self.default_joint_angles = default_joint_angles
+        self.scaling_factors = scaling_factors
 
         # Limits and conditions for the MPS
-        self.jmax_compare = np.array([max_pos[0],max_pos[2],max_pos[3],max_pos[5],max_pos[6],max_pos[8],max_pos[9],max_pos[11]])
-        self.jmin_compare = np.array([min_pos[0],min_pos[2],min_pos[3],min_pos[5],min_pos[6],min_pos[8],min_pos[9],min_pos[11]])
+        self.jmax_compare = np.array([max_pos[0], max_pos[2], max_pos[3], max_pos[5], max_pos[6], max_pos[8],
+                                      max_pos[9], max_pos[11]])
+        self.jmin_compare = np.array([min_pos[0], min_pos[2], min_pos[3], min_pos[5], min_pos[6], min_pos[8],
+                                      min_pos[9], min_pos[11]])
         self.X_safe = 0.1 # Minimum height not to consider a fall for trunk and hips
         self.N_mps = 50 # Number of MPS simulation steps to decide which policy to use
         self.X_inv = 10e-2 # Maximum velocity to consider the robot has stopped
-
-        # Read parameters for the backup policy from the configuration file
-        self.lim_tau = config['backup']['robot']['torque_limit']
-        self.lim_vel = config['backup']['robot']['vel_limit']
-        self.Kp_b = config['backup']['robot']['Kp_b']
-        self.Kd_b = config['backup']['robot']['Kd_b']
+        self.lim_tau = 40
+        self.lim_vel = 26.5
+        self.contact_height = 0.03
+        self.feet_geom = [12, 20, 36, 28]
 
         # Model robot using MuJoCo for the MPS loop
         self.model, self.data = modelData()
 
-        # Load backup network
-        device = torch.device('cpu')
-        if torch.cuda.is_available():
-                device = torch.device('cuda')
-        self.backup_nn = load_backup_nn(config, device)
-
-    def isRecSingle(self, qDes, pose, twist, joint_pos, joint_vel):
+    def is_rec_single(self, qDes, pose, twist, joint_pos, joint_vel, sim_nn, previous_actions, current_actions):
         iter_mps = 0
-
-        data = self.data
-        data.qacc_warmstart = 0
         
         # Define initial data for simulation
-        data.qpos = np.concatenate((pose, joint_pos))
-        data.qvel = np.concatenate((twist, joint_vel))
+        self.data.qpos = np.concatenate((pose, self.swap_legs(joint_pos)))
+        self.data.qvel = np.concatenate((twist, self.swap_legs(joint_vel)))
+        mujoco.mj_forward(self.model, self.data)
 
         ## Trunk, hip and knee positions
-        z_coordinates = np.array([data.body('trunk').xpos[2], data.body('FL_hip').xpos[2], data.body('FR_hip').xpos[2],
-                                data.body('RL_hip').xpos[2], data.body('RR_hip').xpos[2]])
+        z_coordinates = np.array([self.data.body('trunk').xpos[2], self.data.body('FL_hip').xpos[2], self.data.body('FR_hip').xpos[2],
+                                self.data.body('RL_hip').xpos[2], self.data.body('RR_hip').xpos[2]])
         
-        data_compare = np.array([data.qpos[7],data.qpos[9],data.qpos[10],data.qpos[12],data.qpos[13],data.qpos[15],data.qpos[16],data.qpos[18]])
+        data_compare = np.array([self.data.qpos[7], self.data.qpos[9], self.data.qpos[10], self.data.qpos[12],
+                                 self.data.qpos[13], self.data.qpos[15], self.data.qpos[16], self.data.qpos[18]])
         check_pos = (np.any(data_compare > self.jmax_compare) or np.any(data_compare < self.jmin_compare))
 
-        if np.any(z_coordinates < self.X_safe) or np.any(np.abs(data.qvel[6:]) > self.lim_vel) or check_pos: ## x is not in X_safe
+        if np.any(z_coordinates < self.X_safe) or np.any(np.abs(self.data.qvel[6:]) > self.lim_vel) or check_pos: ## x is not in X_safe
             return False, iter_mps
         
         ## Simulate x with pi_hat
         j = 0
-        q_muj = data.qpos.copy()
-        v_muj = data.qvel.copy()
-        torque_values = 4*self.torque_values
+        q_muj = self.data.qpos.copy()
+        v_muj = self.data.qvel.copy()
       
         while j < self.iter_ctrl:
-            u_nominal = self.Kd_n * (- v_muj[6:]) + self.Kp_n * (qDes - q_muj[7:]) +  torque_values
-            data.ctrl = np.clip(u_nominal, -self.lim_tau, self.lim_tau)
+            u_nominal = self.Kd_n * (- v_muj[6:]) + self.Kp_n * (qDes - q_muj[7:]) + self.torque_values
+            self.data.ctrl = np.clip(u_nominal, -self.lim_tau, self.lim_tau)
             j += 1
-            mujoco.mj_step(self.model, data)
-            q_muj = data.qpos.copy()
-            v_muj = data.qvel.copy()
-        
-        for i in range(0, self.N_mps): ##simulated steps
-            z_coordinates = np.array([data.body('trunk').xpos[2], data.body('FL_hip').xpos[2], data.body('FR_hip').xpos[2],
-                                    data.body('RL_hip').xpos[2], data.body('RR_hip').xpos[2]])
-            data_compare = np.array([data.qpos[7],data.qpos[9],data.qpos[10],data.qpos[12],data.qpos[13],data.qpos[15],data.qpos[16],data.qpos[18]])
-            check_pos = (np.any(data_compare > self.jmax_compare) or np.any(data_compare < self.jmin_compare))
-            
-            if np.all(np.abs(data.qvel) <= self.X_inv): ## x is in X_inv
-                return True, iter_mps
-        
-            elif np.any(z_coordinates < self.X_safe) or np.any(np.abs(data.qvel[6:]) > self.lim_vel) or check_pos: ## x is not in X_safe
-                return False, iter_mps
-            
-            # Simulate x with pi_rec
-            imu_acc = data.sensor('Body_Acc').data.copy()
-            imu_acc[2] = -imu_acc[2]
-            imu_acc[1] = -imu_acc[1]
+            mujoco.mj_step(self.model, self.data)
+            q_muj = self.data.qpos.copy()
+            v_muj = self.data.qvel.copy()
 
-            pos_backup_order, last_action = computeBackup(q_muj[7:], v_muj[6:], self.backup_nn, imu_acc, last_action)
+        nominal = False
+        for i in range(0, self.N_mps): ##simulated steps
+            z_coordinates = np.array([self.data.body('trunk').xpos[2], self.data.body('FL_hip').xpos[2], self.data.body('FR_hip').xpos[2],
+                                      self.data.body('RL_hip').xpos[2], self.data.body('RR_hip').xpos[2]])
+            data_compare = np.array([self.data.qpos[7], self.data.qpos[9], self.data.qpos[10], self.data.qpos[12],
+                                     self.data.qpos[13], self.data.qpos[15], self.data.qpos[16], self.data.qpos[18]])
+            check_pos = (np.any(data_compare > self.jmax_compare) or np.any(data_compare < self.jmin_compare))
+
+            xy_coords = []
+
+            phase_all = np.all(np.array(
+                [self.data.geom(self.feet_geom[0]).xpos[2], self.data.geom(self.feet_geom[1]).xpos[2],
+                 self.data.geom(self.feet_geom[2]).xpos[2],
+                 self.data.geom(self.feet_geom[3]).xpos[2]]) < self.contact_height)
+            if phase_all:  # Check that the four feet are in contact with the floor
+                for k in self.feet_geom:
+                    xy_coords.append([self.data.geom(k).xpos[0], self.data.geom(k).xpos[1]])
+
+                if self.capture_point_check(self.data, xy_coords):
+                    return True, iter_mps
+
+        
+            if np.any(z_coordinates < self.X_safe) or np.any(np.abs(self.data.qvel[6:]) > self.lim_vel) or check_pos: ## x is not in X_safe
+                return False, iter_mps
+
+            ## Simulate x with pi_rec
+            new_actions1 = self.compute_actions(self.data, self.scaling_factors, previous_actions, sim_nn)
+
+            previous_actions = current_actions
+            current_actions = self.swap_legs(new_actions1)
+            qDes = self.scaling_qdes * current_actions + np.array(self.default_joint_angles)
+            qDes = np.clip(qDes, self.min_pos, self.max_pos)
             iter_mps += 1
             
             j = 0
             while j < self.iter_ctrl:
                 j += 1
-                u_backup = self.Kd_b * (- v_muj[6:]) + self.Kp_b * (pos_backup_order - q_muj[7:])
-                data.ctrl = np.clip(u_backup, -self.lim_tau, self.lim_tau)
-
-                mujoco.mj_step(self.model, data)
-                q_muj = data.qpos.copy()
-                v_muj = data.qvel.copy()
+                u_nominal = self.Kd_n * (- v_muj[6:]) + self.Kp_n * (qDes - q_muj[7:]) + self.torque_values
+                self.data.ctrl = np.clip(u_nominal, -self.lim_tau, self.lim_tau)
+                mujoco.mj_step(self.model, self.data)
+                q_muj = self.data.qpos.copy()
+                v_muj = self.data.qvel.copy()
 
 
         return False, iter_mps
+
+    def swap_legs(self, array):
+        """
+        Swap the front and rear legs of the array based on predefined indices.
+
+        The swap logic is fixed:
+        - Swap front legs (indices 3:6) with (0:3)
+        - Swap rear legs (indices 9:12) with (6:9)
+        """
+        array_copy = array.copy()  # Make a copy to avoid modifying the original array
+        order = [3, 4, 5, 0, 1, 2, 9, 10, 11, 6, 7, 8]
+        return array_copy[order]
+
+    def compute_observation(self, data_muj, scaling_factors, prev_actions1):
+        """
+        Compute the observation vector from the robot's state.
+        """
+        imu_quat = data_muj.sensor('Body_Quat').data.copy()
+        imu_gyro = data_muj.sensor('Body_Gyro').data.copy()
+
+        commands = np.array([-0.35, -0.1, 0.])
+
+        body_quat = np.array([imu_quat[1], imu_quat[2], imu_quat[3], imu_quat[0]])
+        body_vel = np.array([imu_gyro[0], imu_gyro[1], imu_gyro[2]])
+
+        joint_angles1 = data_muj.qpos.copy()[7:]
+        joint_angles = self.swap_legs(joint_angles1)
+        joint_velocities1 = data_muj.qvel.copy()[6:]
+        joint_velocities = self.swap_legs(joint_velocities1)
+
+        # Gravity vector in body frame
+        gravity_body = quat_rotate_inverse(
+            torch.tensor(body_quat, dtype=torch.float32).unsqueeze(0),
+            torch.tensor([[0.0, 0.0, -1.0]], dtype=torch.float32)
+        ).squeeze().numpy()
+
+        prev_actions = self.swap_legs(prev_actions1)
+
+        # Scale observations
+        scaled_body_vel = body_vel * scaling_factors['body_ang_vel']
+        scaled_commands = commands[:2] * scaling_factors['commands']
+        scaled_commands = np.append(scaled_commands, commands[2] * scaling_factors['body_ang_vel'])
+        scaled_gravity_body = gravity_body * scaling_factors['gravity_body']
+        scaled_joint_angles = np.array(joint_angles) * scaling_factors['joint_angles']
+        scaled_joint_velocities = np.array(joint_velocities) * scaling_factors['joint_velocities']
+        scaled_actions = prev_actions * scaling_factors['actions']
+
+        # Concatenate into a single observation vector
+        return np.concatenate((scaled_body_vel, scaled_commands, scaled_gravity_body, scaled_joint_angles,
+                               scaled_joint_velocities, scaled_actions))
+
+    def compute_actions(self, data_muj, scaling_factors, previous_actions, sim_nn):
+        obs = self.compute_observation(data_muj, scaling_factors, previous_actions)
+        obs_tensor = torch.tensor(obs, dtype=torch.float32)
+        obs_normalized = sim_nn.norm_obs(obs_tensor)
+        with torch.no_grad():
+            new_actions1 = sim_nn(obs_normalized).numpy()
+        return new_actions1
+
+    def capture_point_check(self, data_muj, xy_coords, factor=0.7):
+        # Compute the capture point coordinates
+        com_coordinates = data_muj.body('trunk').subtree_com
+        mujoco.mj_subtreeVel(data_muj.model, data_muj)
+        com_velocities = data_muj.body('trunk').subtree_linvel
+        omega = np.sqrt(abs(data_muj.model.opt.gravity[2]) / com_coordinates[2])
+        cp_x = com_coordinates[0] + (com_velocities[0] / omega)
+        cp_y = com_coordinates[1] + (com_velocities[1] / omega)
+
+        # Define convex hull and shrink it
+        hull_path = Path(xy_coords)
+        polygon = Polygon(hull_path.vertices)
+        hull_path = hull_path.transformed(matplotlib.transforms.Affine2D().scale(factor))
+        shrinked_polygon = Polygon(hull_path.vertices)
+
+        translate_x = polygon.centroid.x - shrinked_polygon.centroid.x
+        translate_y = polygon.centroid.y - shrinked_polygon.centroid.y
+        hull_path = hull_path.transformed(matplotlib.transforms.Affine2D().translate(translate_x, translate_y))
+
+        return hull_path.contains_point((cp_x, cp_y))
